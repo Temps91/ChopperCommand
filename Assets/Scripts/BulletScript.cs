561edc56112086ae85c17adf13716dfe02853adb
Temps91 <jorgebd3045@gmail.com>
2025-10-29 09:33:51 -0600

    Subiendo movimiento bien

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float speed;
+    public Vector2 direction = Vector2.right;
 
+
+
+    public void SetDirection(Vector2 dir)
+    {
+        direction = dir.normalized;
+
+        float angle = direction.x > 0 ? 0f : 180f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
     public void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/ScriptsPlayer/PlayerMovement.cs
+++ Assets/Scripts/ScriptsPlayer/PlayerMovement.cs
@@ -1,15 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public GameObject bulletPrefab;
-    public GameObject shootPoint;
+    public Transform shootPoint;
+    public bool direccionSprite;
+    public bool timeNextShoot;
+    public GameObject helicoptero1;
+    public GameObject helicoptero2;
+    public bool spriteOn;
 
+
+
+    public void Start()
+    {
+        direccionSprite = true;
+        timeNextShoot = false;
+        spriteOn = false;
+        StartCoroutine(ActSprite());
+    }
     public void FixedUpdate()
     {
         Movimiento();
         Shoot();
+
     }
     public void Movimiento()
     {
@@ -17,13 +33,55 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontal, vertical, 0f);
         transform.Translate(movement * speed * Time.deltaTime);
+        if (horizontal > 0 && !direccionSprite)
+        {
+            Flip();
+        }
+        else if (horizontal < 0 && direccionSprite)
+        {
+            Flip();
+        }
     }
 
     public void Shoot()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKey(KeyCode.K) && !timeNextShoot)
         {
-            Instantiate(bulletPrefab, shootPoint.transform.position, shootPoint.transform.rotation);
+            GameObject bullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+            Vector2 direction = direccionSprite ? Vector2.right : Vector2.left;
+            bullet.GetComponent<BulletScript>().SetDirection(direction);
+            timeNextShoot = true;
+            StartCoroutine(ShootTime());
         }
     }
+
+    public void Flip()
+    {
+        direccionSprite = !direccionSprite;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        
+    }
+
+    IEnumerator ShootTime()
+    {
+        yield return new WaitForSeconds(0.4f);
+        timeNextShoot = false;
+    }
+    
+    IEnumerator ActSprite()
+    {
+        while(true)
+        {
+            helicoptero1.SetActive(true);
+            helicoptero2.SetActive(false);
+            yield return new WaitForSeconds(0.2f);
+            helicoptero1.SetActive(false);
+            helicoptero2.SetActive(true);
+            yield return new WaitForSeconds(0.2f);
+        }
+
+    }
+
 }
